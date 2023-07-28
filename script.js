514c63b360faeae6fcc6555e798840ecbfa2faba
Faheem Ud Din <118898064+Faheem798@users.noreply.github.com>
2023-07-28 18:51:33 +0500

    Update script.js
    errors has been solved, saving data in csv file.

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
 
 (async () => {
   try {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://www.daraz.pk/catalog/?from=filter&q=pet%20home&rating=4&ppath=30129:3736');
-    let title = "Null"
-    let price = 'Null'
-    let img = 'Null'
-    const productsHandles = await page.$$('.ant-col-20.ant-col-push-4.side-right--Tyehf')
-    for(const productsHandle of productsHandles){
-      title = await page.evaluate((el)=> el.querySelector('div.info--ifj7U > div.title--wFj93 > a').textContent, productsHandle      )
-      console.log(title)
-      price = await page.evaluate((el)=> el.querySelector('div.info--ifj7U > div.price--NVB62 > span').textContent, productsHandle      )
-      console.log(price)
-      img = await page.evaluate((el)=> el.querySelector('div.img--VQr82 > div > a > img').getAttribute('src'), productsHandle      )
-      console.log(img)
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+      args: ["--start-maximized"],
+    });
+    let items = [];
 
+    const page = await browser.newPage();
+    await page.goto(
+      "https://www.daraz.pk/catalog/?from=filter&q=pet%20home&rating=4&ppath=30129:3736",
+      {
+        waitUntil: "domcontentloaded", // Wait until the DOM content is loaded
+        timeout: 60000, // Increase the navigation timeout to 60 seconds
+      }
+    );
+
+    const productHandles = await page.$$(".gridItem--Yd0sa");
+
+    for (const productHandle of productHandles) {
+      let title = await productHandle.$eval(
+        "div.info--ifj7U > div.title--wFj93 > a",
+        (el) => el.textContent
+      );
+      console.log("Title:", title.trim());
+
+      let price = await productHandle.$eval(
+        "div.info--ifj7U > div.price--NVB62 > span",
+        (el) => el.textContent
+      );
+      console.log("Price:", price.trim());
+
+      let img = await productHandle.$eval(
+        "div.img--VQr82 > div > a > img",
+        (el) => el.getAttribute("src")
+      );
+      console.log("Image:", img);
+
+      items.push({ title, price, img });
+      fs.appendFile('result.csv', `${title},${price},${img}\n`, (err) => {
+        if (err) throw err;
+      });
     }
+
+    console.log("Total items:", items.length);
+
     await browser.close();
   } catch (error) {
     console.error("Error occurred:", error);
